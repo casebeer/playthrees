781d96a301e4798ce5cce0dc6ead44b53bd09e6e
Joe Versoza <christopher@chc.name>
2011-05-21 21:25:47 -0400

    added some functional stuff in utils, added stub for score

---
+++ Score.js
@@ -0,0 +1,9 @@
+function Score(config) {
+	var options = {zero:3}
+	}
+
+Score.prototype.caculateScore = function(dice) {
+	var score;
+	reduce(function() {
+	}, this.score)
+}
--- util.js
+++ util.js
@@ -14,3 +14,27 @@ function apply(defaults, config) {
 	}
 	return ret;
 }
+
+function forEach(a, f) {
+	var i;
+	for (i in a) {
+		if (a.hasOwnProperty(i)) {
+			f(a[i]);
+		}
+	}
+}
+
+function map(f, a) {
+	var result = []
+	forEach(a, function(e) {
+		result.push(f(e));
+	}); 
+	return result;
+}
+
+function reduce(combine, base, a) {
+	forEach(a, function(x) {
+		base = combine(base, x);
+	});
+	return base;
+}
