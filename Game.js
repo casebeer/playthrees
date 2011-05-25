375f2ff1a7e9ae5758b687e78e4dfbee4bb14e80
Christopher H. Casebeer <christopher@chc.name>
2011-05-25 09:43:43 -0400

    Merge in Game.js global game stubs.

--- Game.js
+++ Game.js
@@ -1,33 +1,48 @@
+<<<<<<< Updated upstream
 function Game(config) {
 	var i;
 	this.options = apply({numDice:5}, config);
+	//this.scoreboard = new ScoreBoard();
+
 	this.curPlayer;
 	this.players = [];	
+
 	this.inPlay = [];
 	for (i = 0; i < this.options.numDice; i++) {
 		this.inPlay[i] = new Die();
 	}
 	this.pinned = [];
+	this.toPin = [];
 }
 
-Game.prototype.addPlayer(name) {
+Game.prototype.addPlayer = function (name) {
 	this.players = {name:name};
 }
 
-Game.prototype.start = function() {
+Game.prototype.start = function () {
 	curPlayer = 0;
 }
 
-Game.prototype.nextTurn = function() {
+Game.prototype.nextTurn = function () {
 
 };
 
-Game.prototype.roll = function() {
-	for (die in this.inPlay) {
-		die.roll()
+Game.prototype.roll = function () {
+	// also check not first roll in turn
+	if (this.toPin.length == 0) {
+		throw "Cannot roll without pinning at least one die.";
 	}
-};
 
-Game.prototype.pin = function(die) {
+	forEach(this.toPin, function (d) { 
+		removeItem(this.toPin, d);
+		this.pinned.append(d);
+	});
+
+	forEach(this.inPlay, function (d) {
+		d.roll();
+	}
+}
+
+Game.prototype.pin = function (die) {
 	
 }
